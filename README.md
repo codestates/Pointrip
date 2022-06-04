b9291f40622d8e3d58f5fb9c53161c036795c84b
Johnny <77973029+qkralsrl11@users.noreply.github.com>
2022-06-04 16:19:01 +0900

    Revert "add post order retrieval, image edit, etc"

--- README.md
+++
@@ -1,1 +0,0 @@
-# Pointrip_Server
--- src/controllers/user/logout.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-import { StatusCodes } from "http-status-codes";
-
-export default (req: Request, res: Response) => {
-  console.log('Commencing logout...');
-  res.status(StatusCodes.RESET_CONTENT).cookie('jwt', '').json({ logoutSuccess: true });
-  return console.log('Logged out.');
-}
