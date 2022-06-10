d3f8150c9d5723b95ecfd60a55fe5714e1a9dab0
Johnny <77973029+qkralsrl11@users.noreply.github.com>
2022-06-10 20:00:11 +0900

    Update data-source.ts

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -5,10 +5,10 @@ import Post from "./entity/post"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: "pointrip.czzigxvcggcg.us-east-1.rds.amazonaws.com",
     port: 3306,
-    username: "root",
-    password: "newfox11",
+    username: "pointrip",
+    password: "pointrip11",
     database: "pointrip2",
     synchronize: true,
     logging: true,
