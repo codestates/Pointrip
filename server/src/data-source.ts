efe685103ef6608cedd9060eb316d88728fd5107
Johnny <77973029+qkralsrl11@users.noreply.github.com>
2022-06-11 17:52:54 +0900

    Update data-source.ts

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -2,17 +2,15 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import Post from "./entity/post"
 
-
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: "pointrip.czzigxvcggcg.us-east-1.rds.amazonaws.com",
     port: 3306,
-    username: "root",
-    password: "newfox11",
-    database: "pointrip3",
+    username: "pointrip",
+    password: "pointrip11",
+    database: "pointrip2",
     synchronize: true,
     logging: true,
     entities: ["src/entity/*.{js,ts}"],
 
 })
-
