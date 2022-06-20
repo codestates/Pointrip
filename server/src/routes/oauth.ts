11c2421d22d0cd74395daf028999ce22f8325456
Johnny <77973029+qkralsrl11@users.noreply.github.com>
2022-06-20 22:27:24 +0900

    Revert "server"

--- server/src/routes/oauth.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-import kakao from '../controllers/oauth/kakao';
-
-router.get('/kakao', kakao.requestAuthCode);
-router.get('/', kakao.requestToken);
-
-export default router;
