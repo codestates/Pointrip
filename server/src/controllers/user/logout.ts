357a5608eb89e9dcb70101381f35e1e3bef35a53
Johnny <77973029+qkralsrl11@users.noreply.github.com>
2022-06-20 22:27:44 +0900

    Revert "[Back-end]"

--- server/src/controllers/user/logout.ts
+++
@@ -1,6 +0,0 @@
-import { StatusCodes } from "http-status-codes";
-
-export default (req: any, res: any) => {
-  // TODO: 로그아웃 로직을 작성합니다.
-  return res.status(StatusCodes.RESET_CONTENT).cookie('jwt', '').json({ logoutSuccess: true });
-}
